449de03faa261fe36fe0c25b4e5ebcf1bb4fd225
hanifrev <hanifrev@gmail.com>
2021-10-13 19:51:12 +0700

    feat: add footer

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+const footer = () => {
+    return (
+        <div className="footer-container">
+            <div className="footer">
+                <p style={{color: '#ffffff'}}>this is footer</p>
+            </div>
+        </div>
+    )
+}
+
+export default footer
