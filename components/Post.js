6e083966f884acc542ba51dba5da14f66f2cee4f
hanifrev <hanifrev@gmail.com>
2022-02-16 09:24:42 +0700

    update

--- components/Post.js
+++ components/Post.js
@@ -1,16 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link'
+import Link from "next/link";
 
-export default function Post({post}) {
-    return (
-        <div className="card">
-            <img src={post.frontmatter.cover_image} alt="x" />
-            <div>Posted on {post.frontmatter.date}</div>
-            <h3>{post.frontmatter.title}</h3>
-            <p>{post.frontmatter.excerpt}</p>
-            <Link href={`/blog/${post.slug}`}>
-                <a className='btn'>Read More</a>
-            </Link>
-        </div>
-    )
+export default function Post({ post }) {
+  return (
+    <div className="card">
+      <img src={post.frontmatter.cover_image} alt="x" />
+      <div>Posted on {post.frontmatter.date}</div>
+      <h3>{post.frontmatter.title}</h3>
+      <p>{post.frontmatter.excerpt}</p>
+      <Link href={`/blog/${post.slug}`}>
+        <a className="btn">Read More</a>
+      </Link>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,71 @@
 /* eslint-disable react/no-children-prop */
 /* eslint-disable react/jsx-key */
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import Head from 'next/head'
-import Post from '../components/Post'
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Head from "next/head";
+import Post from "../components/Post";
+import { sortByDate } from "../utils";
 // import TextLoop from "react-text-loop";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
-export default function Home({posts}) {
+export default function Home({ posts }) {
   // const city = ["Kediri, Indonesia", "Taipe, Taiwan", "Madrid, Spain", "Abuja, Nigeria", "Barrow, UK", "New York City, USA"]
-  console.log(posts)
+  console.log(posts);
   return (
     <div>
       <Head>
         <title>test test</title>
       </Head>
-    {/* 
+      {/* 
       <div className="textloop">
         Made with{" "}<FontAwesomeIcon icon={faHeart} color="red" />{" "}
         <TextLoop children={city} interval={2600} fade="true" />
       </div> */}
 
-      <div className='posts'>
-        {posts.map((post, index) => (
+      <div className="posts">
+        {posts.map((thepost, index) => (
           // <h3>{post.frontmatter.title}</h3>
-          <Post post={post} />
+          <Post post={thepost} key={index} />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
   // get files from post directory
-  const files = fs.readdirSync(path.join('posts'))
+  const files = fs.readdirSync(path.join("posts"));
   // console.log(files)
 
   // get slug and frontmatter from post
-  const posts = files.map(filename =>{
+  const posts = files.map((filename) => {
     // create slug
-    const slug = filename.replace('.md', '')
+    const slug = filename.replace(".md", "");
 
     // get frontmatter
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
 
-    const {data:frontmatter} = matter(markdownWithMeta)
+    const { data: frontmatter } = matter(markdownWithMeta);
 
-    console.log(markdownWithMeta)
-    console.log(__dirname)
+    console.log(markdownWithMeta);
+    console.log(__dirname);
 
     return {
       slug,
-      frontmatter
-    }
-  })
+      frontmatter,
+    };
+  });
 
-  console.log(posts)
+  // console.log(posts)
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate),
     },
-  }
+  };
 }
